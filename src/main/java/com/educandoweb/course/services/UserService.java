08b25bc35ac6da4b2973257b39c39497a7752b63
Victor <victor_brandao@outlook.com>
2024-05-20 17:19:11 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -23,4 +23,9 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+//	save() retorna objeto salvo por padrao
+ 	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }
