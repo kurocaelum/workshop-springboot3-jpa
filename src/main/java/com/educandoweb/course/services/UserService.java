aa932787b902546830fc8febf552fcd1a570c771
Victor <victor_brandao@outlook.com>
2024-05-31 14:52:33 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -49,11 +51,15 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 // 		getReferenceById() instancia um usuario monitorado pelo JPA, mas nao vai no banco de dados ainda
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-
-		return repository.save(entity);
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
